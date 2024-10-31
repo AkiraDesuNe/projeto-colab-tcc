ba96085074bc636945710073ca03a0673af6b556
Akira アキラ <143705327+AkiraDesuNe@users.noreply.github.com>
2024-10-31 11:56:57 +0000

    Add colab-1.py

---
+++ colab-1.py
@@ -0,0 +1,24 @@
+import os
+
+print("Akira") 
+
+os.system ("cls || clear")
+
+print ("Colaborador - 1")
+
+# COMANDOS DO GIT
+
+"Atualiza o repositório atual com dado do repositório no GitHub"
+# git pull
+
+"Adiciona novos dados para o commit "
+# git add.
+
+"Versiona com os novos dados "
+# git commit -m "Título para  commit"
+
+"Atualiza trazendo commits do GitHub."
+# git pull
+
+"Envia os commits para o GitHub"
+# git push
